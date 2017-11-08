b43f81dee8ece9b0f84e1077418db97b3de580d8
leejuheon <dydnrn12@naver.com>
2017-11-09 01:01:24 +0900

    modified climbstair

--- climbstair.cpp
+++ climbstair.cpp
@@ -2,21 +2,21 @@
 #include <algorithm>
 
 using namespace std;
-
+//DP problem 
 int main(){
 
-	int n;
-	int st[305] = {};
-	int dp[305] = {};
+	int n; //number of stairs
+	int st[305] = {}; //number of entire stairs , each has the stair value.
+	int dp[305] = {}; //each stair's max sum.
 	cin>>n;
 	for(int i=1; i<=n; i++)
 		cin>>st[i];
 		
-	dp[1] = st[1];
-	dp[2] = st[1] + st[2];
+	dp[1] = st[1]; //the entry value
+	dp[2] = st[1] + st[2]; // entry value
 	
 	for(int j=3; j<=n; j++)
-	dp[j] = max((dp[j-2]+st[j]),(dp[j-3]+st[j-1]+st[j]));
+	dp[j] = max((dp[j-2]+st[j]),(dp[j-3]+st[j-1]+st[j])); //recursion fomula
 	
 	cout<<"max:"<<dp[n]<<endl;
 }
